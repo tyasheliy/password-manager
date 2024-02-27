3fbb5c8deebace46182bfbc6dca27f570d106c75
Igor228337 <Dj.igo@yandex.ru>
2024-02-27 21:07:37 +0300

    Igor

---
+++ DB.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+
+
+namespace KeyLoger
+{
+    public class UserData
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+        public string PasswordMasked => new string('*', Password.Length);
+    }
+    public class SQLiteManager
+    {
+        private readonly string _connectionString;
+
+        public SQLiteManager(string databaseFilePath)
+        {
+            _connectionString = $"Data Source=./{databaseFilePath};";
+        }
+
+        public void AddItem(string tableName, Dictionary<string, object> data)
+        {
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+
+                var commandText = $"INSERT INTO {tableName} ({string.Join(", ", data.Keys)}) VALUES ({string.Join(", ", new string[data.Count].Select((s, i) => $"@param{i}"))})";
+                using (var command = new SQLiteCommand(commandText, connection))
+                {
+                    int i = 0;
+                    foreach (var item in data)
+                    {
+                        command.Parameters.AddWithValue($"@param{i}", item.Value);
+                        i++;
+                    }
+
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public List<Dictionary<string, object>> ExecuteCustomQuery(string query)
+        {
+            var resultList = new List<Dictionary<string, object>>();
+
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+                using (var command = new SQLiteCommand(query, connection))
+                {
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            var row = new Dictionary<string, object>();
+                            for (int i = 0; i < reader.FieldCount; i++)
+                            {
+                                row[reader.GetName(i)] = reader.GetValue(i);
+                            }
+                            resultList.Add(row);
+                        }
+                    }
+                }
+            }
+
+            return resultList;
+        }
+        public void DeleteItem(string tableName, string columnName, string columnValue)
+        {
+            using (var connection = new SQLiteConnection(_connectionString))
+            {
+                connection.Open();
+
+                var commandText = $"DELETE FROM {tableName} WHERE {columnName} = @value";
+                using (var command = new SQLiteCommand(commandText, connection))
+                {
+                    command.Parameters.AddWithValue("@value", columnValue);
+                    command.ExecuteNonQuery();
+                }
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace KeyLoger
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillGrid();
+        }
+
+        private void FillGrid()
+        {
+            List<UserData> list = new List<UserData>();
+            SQLiteManager sQLiteManager = new SQLiteManager($"./db.db");
+            var data = sQLiteManager.ExecuteCustomQuery("SELECT word, password FROM Keywords");
+            foreach (Dictionary<string, object> item in data)
+            {
+                string login = item["word"] as string;
+                string password = item["password"] as string;
+
+                list.Add(new UserData { Login = login, Password = password });
+            }
+            MainGrid.ItemsSource = list;
+        }
+
+        private void MainGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void CopyBTN_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainGrid.SelectedItem is UserData selectedUser)
+            {
+                Clipboard.SetText(selectedUser.Password);
+
+                MessageBox.Show($"Значение '{selectedUser.Password}' скопировано в буфер обмена.");
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите строку.");
+            }
+        }
+
+        private void DeleteBTN_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedUser = MainGrid.SelectedItem as UserData;
+            if (selectedUser != null)
+            {
+                var sQLiteManager = new SQLiteManager("./db.db");
+                sQLiteManager.DeleteItem("Keywords", "word", selectedUser.Login);
+
+                // Удаление выбранного элемента из списка
+                var list = MainGrid.ItemsSource as List<UserData>;
+                if (list != null)
+                {
+                    list.Remove(selectedUser);
+                    MainGrid.ItemsSource = null;
+                    MainGrid.ItemsSource = list;
+
+                    MessageBox.Show("Выбранная строка и соответствующая запись в базе данных успешно удалены.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, выберите строку для удаления.");
+            }
+        }
+    }
+}
